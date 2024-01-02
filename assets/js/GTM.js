2425e9193e34a7c714ba7d269c7e0fcf7f3940f3
PikiranKita | Media Penulisan dan Edukasi Pemikiran <72850308+ajax-jquery@users.noreply.github.com>
2024-01-02 18:12:52 +0800

    Update and rename GTM.js to GTM-P6VLR58Z.js

--- assets/js/GTM.js
+++
@@ -1,1 +0,0 @@
-!function(e,t,a,r,g){e[r]=e[r]||[],e[r].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var n=t.getElementsByTagName(a)[0],s=t.createElement(a);s.async=!0,s.src="https://www.googletagmanager.com/gtm.js?id="+g+("dataLayer"!=r?"&l="+r:""),n.parentNode.insertBefore(s,n)}(window,document,"script","dataLayer","GTM-P6VLR58Z");
