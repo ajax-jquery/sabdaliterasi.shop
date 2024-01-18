929c8b0d6e17c4b9159848c13c6697a9d63a31bc
PikiranKita | Media Penulisan dan Edukasi Pemikiran <72850308+ajax-jquery@users.noreply.github.com>
2024-01-19 04:43:29 +0800

    Update Features.js

--- feeds/posts/summary/-/Features.js
+++ feeds/posts/summary/-/Features.js
@@ -53,12 +53,3 @@ showRelatedPost({
     
     
     ]}});
-// API callback    
-autoChapterIndex({
-  "version":"1.0","encoding":"UTF-8","feed":{
-    "xmlns":"http://www.w3.org/2005/Atom","xmlns$openSearch":"http://a9.com/-/spec/opensearchrss/1.0/","xmlns$blogger":"http://schemas.google.com/blogger/2008","xmlns$georss":"http://www.georss.org/georss","xmlns$gd":"http://schemas.google.com/g/2005","xmlns$thr":"http://purl.org/syndication/thread/1.0","id":{"$t":"tag:blogger.com,1999:blog-801953276432701327"},
-    
-    "updated":{"$t":"{{ site.time | date_to_xmlschema }}"},
-    "category":[
-      {"term":"Documentation"},{"term":"Features"},{"term":"Add-ons"},{"term":"Customisation"},{"term":"Fullpage"},{"term":"New in Plus UI"},{"term":"Optimization"},{"term":"Product"},{"term":"Sponsored"},{"term":"Tips"}],
-    "title":{"type":"text","$t":"{{ site.title }}"},"subtitle":{"type":"html","$t":"{{ site.subjudul }}"},"link":[{"rel":"http://schemas.google.com/g/2005#feed","type":"application/atom+xml","href":"{{ site.url }}/feeds\/posts\/summary"},{"rel":"self","type":"application/atom+xml","href":"https:\/\/www.blogger.com\/feeds\/801953276432701327\/posts\/summary\/-\/Features?alt=json-in-script\u0026max-results=2\u0026orderby=updated"},{"rel":"alternate","type":"text/html","href":"{{ site.url }}"},{"rel":"hub","href":"http://pubsubhubbub.appspot.com/"},{"rel":"next","type":"application/atom+xml","href":"https:\/\/www.blogger.com\/feeds\/801953276432701327\/posts\/summary\/-\/Features\/-\/Features?alt=json-in-script\u0026start-index=3\u0026max-results=2\u0026orderby=updated"}],"author":[{"name":{"$t":"Unknown"},"email":{"$t":"noreply@blogger.com"},"gd$image":{"rel":"http://schemas.google.com/g/2005#thumbnail","width":"16","height":"16","src":"https:\/\/img1.blogblog.com\/img\/b16-rounded.gif"}}],"generator":{"version":"7.00","uri":"http://www.blogger.com","$t":"Blogger"},"openSearch$totalResults":{"$t":"6"},"openSearch$startIndex":{"$t":"1"},"openSearch$itemsPerPage":{"$t":"2"}}});
