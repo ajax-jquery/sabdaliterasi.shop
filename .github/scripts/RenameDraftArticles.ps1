50f232de726c05c9a89ff52f252cf6a2c42e40c8
PikiranKita | Media Penulisan dan Edukasi Pemikiran <72850308+ajax-jquery@users.noreply.github.com>
2024-02-11 13:40:43 +0800

    Update RenameDraftArticles.ps1

--- .github/scripts/RenameDraftArticles.ps1
+++ .github/scripts/RenameDraftArticles.ps1
@@ -26,22 +26,13 @@ $ResolvedDraftsPath = Join-Path -Path $BasePath -ChildPath $DraftsPath -Addition
 $ResolvedPostsPath = Join-Path -Path $BasePath -ChildPath $PostsPath
 $DateRegex = '^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])'
 $ShouldPublish = $false
-$TimeZone = 'Asia/Jakarta'  # Tentukan zona waktu Anda di sini
-
-try {
-    $CurrentDate = [System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId((Get-Date),$TimeZone)
-}
-catch {
-    Write-Output "Gagal mengonversi zona waktu. Menggunakan waktu sistem."
-    $CurrentDate = Get-Date
-}
 '::group::Set TimeZone'
 $DefaultTimeZoneMessage = 'Setting TimeZone to default ''Coordinated Universal Time''.'
 try {
     $configContent = Get-Content -Path $ResolvedConfigPath -Raw -ErrorAction Stop
     $config = $configContent | ConvertFrom-Yaml
     if ($config -and $config.timezone) {
-        $TimeZone = $config.timezone
+        $TimeZone = 'Asia/Jakarta'
         'Setting TimeZone from {0} to ''{1}''.' -f $ConfigPath,$TimeZone
     } else {
         $DefaultTimeZoneMessage
