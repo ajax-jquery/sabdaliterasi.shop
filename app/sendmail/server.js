e4851b1f1bdd70f90c40140f6b42ab2679958a2c
PikiranKita | Media Penulisan dan Edukasi Pemikiran <72850308+ajax-jquery@users.noreply.github.com>
2024-11-14 06:39:47 +0800

    Update server.js

--- app/sendmail/server.js
+++ app/sendmail/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: 'app/sendmail/.env' });
 const Parser = require("rss-parser");
 const fetch = require("node-fetch");
 const nodemailer = require("nodemailer");
