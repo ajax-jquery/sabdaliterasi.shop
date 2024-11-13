744539904a5791cde99dc1d0b68bf083177d6314
PikiranKita | Media Penulisan dan Edukasi Pemikiran <72850308+ajax-jquery@users.noreply.github.com>
2024-11-14 06:45:47 +0800

    Update server.js

--- app/sendmail/server.js
+++ app/sendmail/server.js
@@ -8,9 +8,9 @@ const firebaseAdmin = require('firebase-admin');
 // Inisialisasi Firebase
 const serviceAccount = {
   type: "service_account",
-  project_id: "subscribesabda",
+  project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCzR0rAYPxYD8NG\nAQf1zaVDbj3VK1sQfG10QjANpwdokXwmYO8xhadCWqisfbPKeRBlzN0FN4e3Mx7t\nzOWhnHWqNbUKxhl8ktAeAaFWbD/nEU/YpFSof3H2++lr7AAZmF0JlghA86W67ujD\nE3ZKMmBzT2MzMDIwNyhcJu3foe1dcNT6engq+6BU/mCiGLnVK0iEGrgpN0214jWp\n8jkw5pE/PFYpjI4KZSOwS81LbWx1+sRCmByj9t+RIay3WtKZUhw27v8+S14WP6DZ\nR8YnWwwp3dD2PqIkhqiGErwksb0nbk/G831QpbrjeuEhJRKUf1QXAKclTO5ZiC4D\nRpKZ9rdLAgMBAAECggEAQwuNVAaxOEmk0IBANteST3Zh31+YbcDqugiqqeBmL+AA\nmuWzbkS6eWEoJWHZJAZEcJ29pl7O/V872NjAo8C8bEwriXzuOR06gY5AhO50s3hQ\n8Qb7tgv7Vc99yuN2YhwN5zDZo1i7CP6hw5p6qWLPmS/+PD1w94EZMeVJHlITT9bq\ntr7JVdouk2UhhGoh4Oqbbxo/hXWiQk86pyDsD62SzPoAJlUV5GsHRGh9BNCW/G2w\nOKLZm99lw6e7Zxibg40n3a60wl+x2ppkr7sSOyZiWDXdgjFvKzUrR29zrnSj/Jk5\nJ46/OOITcu6VtWBfQ2MJUDGHA31kKFaRa6BFQ+Wr2QKBgQDosA2Q+kw0voOYz5lW\njssn5z+6iFr0lFI39YL0IF4VXKUiAPA2sLih/J8FwHo7GHCwXl/h1cNrKbjfbuCi\nYrAdcH+nkAZbPgtvSMyPmjQ5xxeOMwCeqEmjtrQczQ2E0+LJf7jirPhQR/TVd9JO\nBRPXQvQWfgLlUhkBHxyL6yTIRQKBgQDFPWfQwf4etJ3t66HdOK/yDSIy/uLjx9AN\nLkgb5N2mC+HinCRYVlpmkj4ZKG97Tm27tc+mf28hsDTzm67mCnFeZEP4O3D+RDbq\nsVrNXdF61sFQDwhlGVG/T+u5giZcpECf/xiGG6sIDrkFh9B/8CuLqyBt9wicTvHQ\ntfqd4pDiTwKBgQCj0MZMShr6iyHkpEsE6FTYi/KO5U57oCxeHyeJrXvvOCE91kFH\nj5T83fgquX6UOjmjwV1phfAlcuMn0kytpiLtJvWIR1piREUSnML/WoXoI2VPBFMs\nX7gwXQFiwW4CFJY2XAgS9cl/45jX7JqPuP1l8Eodx6tMPUdDRd74Fpz3rQKBgApd\nc7swtp3+d94s59CsTfK2ZfmwE1RNB83phLiSimNTdIdoFTDfONjzDnhb+5/LO1EF\n+OeTAtAOZnn20HYeTl7bfgbpeMKj6w7iSxdHut5tsrL2V0aY0gbvuD+Da2iq6Qp/\naVaC1bYSr/5uaLKQeHlagNXGiZlNPL/vEECDy4FZAoGBAMU4eRxIH9znlRulCcRL\nu2iGjzx9QLB7PwtngdSrxKEBqwFU1UEh7A0k4B1aGi04Nh1hQ6Vkfx9/wKZjsj4S\n/b3qUb/XnM8o0ghehC9EZvBuxYtO/HUz6pZi7V6YWx9WWJclVlRB4o4Te1oYmpp3\nN/g5P6V+RZ18pvXdWLxO3YsI\n-----END PRIVATE KEY-----\n",
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
